45e4bbac8b9e1617b709e95c926afca3ab9bffc3
kevinmoore444 <kevinmoore444@gmail.com>
2023-01-10 09:06:42 -0800

    COMMIT

--- bubbleSort.js
+++ bubbleSort.js
@@ -27,8 +27,3 @@ const bubbleSort = (nums) => {
 const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 console.log(bubbleSort(numsRandomOrder));
 
-
-// Nested for loop, once you're looping backward, and then you're looping forward.
-// In the forward loop you check if the next value is 
-
-
